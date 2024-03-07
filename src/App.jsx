9fe0bd00d4c755f9d683333b3a284954a151ad0d
EXT02D52540 <serhat.bek@consultant.turkcell.com.tr>
2024-03-07 15:25:10 +0300

    center select

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <section className='section'>
+      <section className='section section-select'>
         <h2>Select</h2>
         <div className='container'>
           <Select
